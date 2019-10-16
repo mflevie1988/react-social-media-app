0835156dc0684b5ac2ff902ab2fc8ec424e7a36d
Ivan Perera <ivan@majestic.com.au>
2019-10-16 12:28:36 +1100

    Sign up and login with validations added

--- functions/index.js
+++ functions/index.js
@@ -1,38 +1,55 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const express  = require('express');
+const app = express();
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello world");
-});
+const config = {
+    apiKey: "AIzaSyAYVYjJDv_RmNNBzSTisHav_fN7MlzRQHs",
+    authDomain: "social-media-app-c1c85.firebaseapp.com",
+    databaseURL: "https://social-media-app-c1c85.firebaseio.com",
+    projectId: "social-media-app-c1c85",
+    storageBucket: "social-media-app-c1c85.appspot.com",
+    messagingSenderId: "927185357789",
+    appId: "1:927185357789:web:1f155e2eaed076dc810434",
+    measurementId: "G-5JJ9GHB8KX"
+};
 
-exports.getScreams = functions.https.onRequest((req, res) => {
-    admin.firestore().collection('screams').get()
+const firebase = require('firebase');
+firebase.initializeApp(config);
+
+const db = admin.firestore();
+
+app.get('/screams', (req, res) => {
+    db
+    .collection('screams')
+    .orderBy('createdAt', 'desc')
+    .get()
         .then(data => {
             let screams = [];
             data.forEach(doc => {
-                screams.push(doc.data());
+                screams.push({
+                    screamId: doc.id,
+                    body: doc.data().body,
+                    userHandler: doc.data().userHandler,
+                    createdAt: doc.data().createdAt
+                });
             });
             return res.json(screams);
         })
         .catch(err => console.error(err));
 });
 
-exports.createScream = functions.https.onRequest((req, res) => {
-    if(req.method !== 'POST'){
-        return res.status(400).json({error: "Request Method not allowed"});
-    }
+app.post('/scream',(req, res) => {
+
     const newScream = {
         body: req.body.body,
         userHandler: req.body.userHandler,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     };
 
-    admin.firestore()
+    db
     .collection('screams')
     .add(newScream)
     .then(doc => {
@@ -43,3 +60,115 @@ exports.createScream = functions.https.onRequest((req, res) => {
         console.error(err);
     });
 });
+
+// Helper methods
+const isEmpty = (string) => {
+    if(string.trim() === '') return true;
+    else return false;
+};
+
+const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email.match(regEx)) return true;
+    else return false;
+};
+
+//Signup route
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email : req.body.email,
+        password : req.body.password,
+        confirmPassword : req.body.confirmPassword,
+        handle : req.body.handle
+    };
+
+    let errors = {};
+
+    //Email validation
+    if(isEmpty(newUser.email)){
+        errors.email = 'Email must not be empty';
+    }else if(!isEmail(newUser.email)){
+        errors.email = 'Must be a valid email address';
+    }
+
+    //Password validation
+    if(isEmpty(newUser.password)) errors.password = 'Must not be empty';
+
+    if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match';
+
+    if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+    //TODO: validate data
+    let userToken, userId;
+    db.doc(`/users/${newUser.handle}`).get()
+        .then(doc => {
+            if(doc.exists){
+                return res.status(400).json({handle: 'This handle has already taken'})
+            }else{
+                return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password);
+            }
+        })
+        .then(data => {
+            userId = data.user.uid;
+            return data.user.getIdToken()
+        })
+        .then(token => {
+            userToken = token;
+            const userCredentials = {
+                handle: newUser.handle,
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
+                userId
+            };
+            return db.doc(`/user/${newUser.handle}`).set(userCredentials);
+            //return res.status(201).json({token})
+        })
+        .then(() => {
+            return res.status(201).json({userToken});
+        })
+        .catch(err => {
+            console.error(err);
+            if(err.code === 'auth/email-already-in-use'){
+                return res.status(400).json({email: 'Email is already in use'});
+            }else{
+                return res.status(500).json({error: err.code});
+            }
+        });
+});
+
+//Login route
+app.post('/login', (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    let errors = {};
+
+    if(isEmpty(user.email)) errors.email = 'Must not be empty';
+    if(isEmpty(user.password)) errors.password = 'Must not be empty';
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then(data => {
+            return data.user.getIdToken();
+        })
+        .then(token => {
+            return res.status(201).json({token});
+        })
+        .catch(err => {
+            console.error(err);
+            if(err.code === 'auth/wrong-password') return res.status(403).json({general: 'Wrong credentials, please try again.'});
+            return res.status(500).json({error: err.code});
+        });
+    
+});
+
+
+
+
+exports.api = functions.region('asia-northeast1').https.onRequest(app);
